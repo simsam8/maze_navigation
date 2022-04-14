478a9f4664bdb16bc6617a6de6536257238e20d0
simsam8 <80512369+simsam8@users.noreply.github.com>
2022-04-14 22:07:29 +0200

    created class that creates a maze from a 2D array

---
+++ main.py
@@ -0,0 +1,80 @@
+import pygame
+from map import lvl1, lvl2
+
+pygame.init()
+
+SCREEN_INFO = pygame.display.Info()
+WIDTH = SCREEN_INFO.current_w*0.8
+HEIGHT = SCREEN_INFO.current_h
+RATIO = WIDTH/HEIGHT
+#print(RATIO)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Mazerunner')
+
+
+
+class Maze():
+    def __init__(self, map) -> None:
+        self.map = map
+        self.map_height = len(self.map)
+        self.map_width = len(self.map[0])
+        self.color = (255,255,255)
+        self.block_height = HEIGHT/self.map_height
+        self.block_width = WIDTH/self.map_width
+        #print(self.block_height, self.block_width)
+        
+    def draw(self):
+        blocks =  []
+        x = 0
+        y = 0
+        
+        r = 0
+        c = 0
+        
+        for row in self.map:
+            for col in row:
+                if col == 1:
+                    blocks.append(pygame.Rect(x, y, self.block_width, self.block_height))
+                else:
+                    pass
+                x += self.block_width
+                c +=1
+                
+                if c == self.map_width:
+                    x = 0
+                    c = 0
+            
+            y += self.block_height
+            r += 1
+            
+            
+            
+            
+        for x in blocks:
+            pygame.draw.rect(screen, self.color, x, width=25)
+        
+    
+    def print_dim(self):
+        print(self.map_width, self.map_height)
+
+lab = Maze(lvl1)
+
+
+def loop():
+    
+    running = True
+    
+    screen.fill((0,0,0))
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return pygame.quit()
+            
+        
+        lab.draw()
+        pygame.display.update()
+            
+if __name__ == '__main__':
+    loop()
